9bd36ba5a1d677d698b0a04d763b360c78590669
Volders Seppe (SVO) <seppe.volders@niko.eu>
2023-12-05 12:15:48 +0100

    add back health potion

--- procgen.py
+++ procgen.py
@@ -66,10 +66,9 @@ def place_entities(
         if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
             item_chance = random.random()
 
-            # if item_chance < 0.7:
-            #     entity_factories.health_potion.spawn(dungeon, x, y)
-            # el
-            if item_chance < 0.8:
+            if item_chance < 0.7:
+                entity_factories.health_potion.spawn(dungeon, x, y)
+            elif item_chance < 0.8:
                 entity_factories.fireball_scroll.spawn(dungeon, x, y)
             elif item_chance < 0.9:
                 entity_factories.confusion_scroll.spawn(dungeon, x, y)
