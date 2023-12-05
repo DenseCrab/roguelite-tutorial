a98340ab5b6c68a98fb37f1b246f6811a879afa0
Volders Seppe (SVO) <seppe.volders@niko.eu>
2023-12-05 12:14:21 +0100

    Part 10 - save and load

--- engine.py
+++ engine.py
@@ -9,6 +9,8 @@ import exceptions
 from message_log import MessageLog
 from render_functions import render_bar, render_names_at_mouse_location
 
+import lzma
+import pickle
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
@@ -22,6 +24,12 @@ class Engine:
         self.mouse_location = (0, 0)
         self.player = player
 
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
+            
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
